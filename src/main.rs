5d8424bc1551df834b2bdf65af1621810843df7c
ldesgoui <ldesgoui@gmail.com>
2018-11-14 05:47:12 +0100

    In The Next Week: motion blur

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,9 @@ const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 fn main() -> Result<(), failure::Error> {
     pretty_env_logger::init();
 
-    let width = 2000;
-    let height = 1000;
-    let samples = 200;
+    let width = 1000;
+    let height = 500;
+    let samples = 50;
 
     info!("version: {}", VERSION);
 
@@ -28,14 +28,17 @@ fn main() -> Result<(), failure::Error> {
         &vec3(0.0, 1.0, 0.0),
         50.0,
         width as f32 / height as f32,
-        0.2,
+        0.0,
         (look_from - look_at).magnitude(),
+        0.0,
+        0.1,
     );
     let world: Vec<Box<Hitable>> = vec![
         Box::new(Sphere {
             center: vec3(0.0, 0.0, -1.0),
             radius: 0.25,
             material: Material::Dielectric { refraction: 4.0 },
+            linear_velocity: Vec3::zeros(),
         }),
         Box::new(Sphere {
             center: vec3(1.0, 0.0, -1.0),
@@ -44,6 +47,7 @@ fn main() -> Result<(), failure::Error> {
                 albedo: vec3(0.8, 0.6, 0.2),
                 fuzz: 0.0,
             },
+            linear_velocity: Vec3::zeros(),
         }),
         Box::new(Sphere {
             center: vec3(-1.0, 0.0, -1.0),
@@ -52,6 +56,7 @@ fn main() -> Result<(), failure::Error> {
                 albedo: vec3(0.6, 0.6, 0.6),
                 fuzz: 1.0,
             },
+            linear_velocity: Vec3::zeros(),
         }),
         Box::new(Sphere {
             center: vec3(0.0, 0.0, -4.0),
@@ -59,6 +64,7 @@ fn main() -> Result<(), failure::Error> {
             material: Material::Lambertian {
                 albedo: vec3(0.1, 0.2, 0.5),
             },
+            linear_velocity: vec3(0.0, 0.5, 0.0),
         }),
         Box::new(Sphere {
             center: vec3(0.0, -100.5, -1.0),
@@ -66,6 +72,7 @@ fn main() -> Result<(), failure::Error> {
             material: Material::Lambertian {
                 albedo: vec3(0.8, 0.8, 0.0),
             },
+            linear_velocity: Vec3::zeros(),
         }),
     ];
 
@@ -78,9 +85,12 @@ fn main() -> Result<(), failure::Error> {
     let buf = image::RgbImage::from_fn(width, height, |x, y| {
         use std::ops::Div;
 
+        if x == 0 {
+            info!("{:5.1}% complete", 100.0 * y as f32 / height as f32);
+        }
+
         (0..samples)
             .fold(vec3(0.0, 0.0, 0.0), |acc, _| {
-                // todo: sample uniformly
                 let u = (rand::random::<f32>() + x as f32) / width as f32;
                 let v = (rand::random::<f32>() + y as f32) / height as f32;
                 acc + color(&world, &camera.ray(u, v), 0)
@@ -137,6 +147,7 @@ fn color(world: &Vec<Box<Hitable>>, ray: &Ray, depth: u32) -> Vec3 {
 struct Ray {
     origin: Vec3,
     direction: Vec3,
+    time: f32,
 }
 
 impl Ray {
@@ -182,11 +193,18 @@ struct Sphere {
     center: Vec3,
     radius: f32,
     material: Material,
+    linear_velocity: Vec3,
+}
+
+impl Sphere {
+    fn center_at(&self, time: f32) -> Vec3 {
+        self.center + self.linear_velocity * time
+    }
 }
 
 impl Hitable for Sphere {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<Hit> {
-        let oc = ray.origin - self.center;
+        let oc = ray.origin - self.center_at(ray.time);
         let a = ray.direction.dot(&ray.direction);
         let b = oc.dot(&ray.direction);
         let c = oc.dot(&oc) - self.radius * self.radius;
@@ -229,6 +247,8 @@ struct Camera {
     u: Vec3,
     v: Vec3,
     lens_radius: f32,
+    t0: f32,
+    t1: f32,
 }
 
 impl Camera {
@@ -240,6 +260,8 @@ impl Camera {
         aspect: f32,
         aperture: f32,
         focus_dist: f32,
+        t0: f32,
+        t1: f32,
     ) -> Self {
         let theta = vfov.to_radians();
         let half_height = (theta / 2.0).tan();
@@ -256,10 +278,14 @@ impl Camera {
             u: u,
             v: v,
             lens_radius: aperture / 2.0,
+            t0: t0,
+            t1: t1,
         }
     }
 
     fn ray(&self, u: f32, v: f32) -> Ray {
+        use rand::Rng;
+
         let rd = self.lens_radius * rand_in_disk();
         let offset = self.u * rd.x + self.v * rd.y;
         Ray {
@@ -268,6 +294,7 @@ impl Camera {
                 - v * self.vertical
                 - self.origin
                 - offset,
+            time: rand::thread_rng().gen_range(self.t0, self.t1),
         }
     }
 }
@@ -290,6 +317,7 @@ impl Material {
                     Ray {
                         origin: hit.point,
                         direction: target - hit.point,
+                        time: ray.time,
                     },
                     albedo,
                 ))
@@ -305,6 +333,7 @@ impl Material {
                     Ray {
                         origin: hit.point,
                         direction: reflected + fuzz * rand_in_sphere(),
+                        time: ray.time,
                     },
                     albedo,
                 ))
@@ -327,6 +356,7 @@ impl Material {
                             Ray {
                                 origin: hit.point,
                                 direction: refracted,
+                                time: ray.time,
                             },
                             attenuation,
                         ));
@@ -336,6 +366,7 @@ impl Material {
                     Ray {
                         origin: hit.point,
                         direction: reflect(ray.direction, hit.normal),
+                        time: ray.time,
                     },
                     attenuation,
                 ))
@@ -401,4 +432,20 @@ fn schlick(cosine: f32, refraction: f32) -> f32 {
     let r0 = (1.0 - refraction) / (1.0 + refraction);
     let r0 = r0 * r0;
     r0 + (1.0 - r0) * (1.0 - cosine).powf(5.0)
+}
+
+fn min<A: PartialOrd>(lhs: A, rhs: A) -> A {
+    if lhs.le(&rhs) {
+        lhs
+    } else {
+        rhs
+    }
+}
+
+fn max<A: PartialOrd>(lhs: A, rhs: A) -> A {
+    if lhs.ge(&rhs) {
+        lhs
+    } else {
+        rhs
+    }
 }
